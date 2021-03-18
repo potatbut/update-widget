0d3e5dca5e56a4e98e2bf7f9a3512579ae1f9b94
vera <darthyoon@gmail.com>
2021-03-18 22:01:11 +0300

    Production update

--- dist/updates-widget.e31bb0bc.js
+++ dist/updates-widget.e31bb0bc.js
@@ -420,7 +420,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62563" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62952" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
